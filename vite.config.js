2dc2f54688eaf17d9514d5d88c0175217e347a21
pijush mishra <pijushkantimishra971@gmail.com>
2024-05-13 20:34:19 +0530

    vite-config file modified2

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["/public/home.png"]
-    }
-  }
+
 })
